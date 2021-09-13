174a87fb330c632d25df76899ddd2e4e6052fcba
Erik J Olsson <ejo@pop-os.localdomain>
2021-09-13 14:20:04 +0200

    Comment

--- Setup.sh
+++ Setup.sh
@@ -1,8 +1,12 @@
 read -p "Link?" Link
 read -p "Token?" Token
+read -p "Email?" Email
+read -p "Username?" Name
 git init
 git add .
 git commit -m "Comment"
 git branch -M main
 git remote add origin $Link
 git push -u origin main
+$Name
+$Token
